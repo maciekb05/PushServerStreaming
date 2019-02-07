780b931431120a5580facc46b735490963c04a40
Maciej <maciej.bielech@gmail.com>
2019-02-07 17:06:22 +0100

    facade in libraries

--- socketLib/index.js
+++ socketLib/index.js
@@ -1,19 +1,27 @@
 const socketIo = require('socket.io');
 var io;
 
-exports.initializeSocket = function initializeSocket(server) {
-    io = socketIo(server)
-} 
+class Facade {
+    constructor () {
+        this.log("Facade just created");
+    }
 
-exports.notifyEveryone = function notifyEveryone(eventName, eventObject) {
-    io.emit(eventName, eventObject);
-}
-
-exports.onEvent = function onEvent(eventName, callback) {
-    io.on('connection', function(socket){
-        socket.on(eventName, function(obj){
-            callback(obj);
+    initializeSocket = function initializeSocket(server) {
+        io = socketIo(server)
+    } 
+    
+    notifyEveryone = function notifyEveryone(eventName, eventObject) {
+        io.emit(eventName, eventObject);
+    }
+    
+    onEvent = function onEvent(eventName, callback) {
+        io.on('connection', function(socket){
+            socket.on(eventName, function(obj){
+                callback(obj);
+            });
         });
-    });
+    }
 }
+
+exports = new Facade();
 
